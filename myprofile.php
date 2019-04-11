26ede83130fdf8533fb48df6263d0e2c8ac81050
Tan-WeiQi <43580636+Tan-WeiQi@users.noreply.github.com>
2019-04-11 21:52:27 +0800

    Add files via upload

--- myprofile.php
+++ myprofile.php
@@ -16,24 +16,26 @@ $rows = mysqli_fetch_assoc($result)
     </head>
     
     <body>
+	<div class="box">
         <center>
             <header><?php echo $rows['display_name']; ?>'s Profile</header>
         
 		     
         
-			<h2> First name: <?php echo $rows['first_name'];  ?> </h2>
-			<h2> Last Name: <?php echo $rows['last_name'];  ?>  </h2>
-            <h2> Gender: <?php echo $rows['gENDer']; ?> </h2>
-			<h2> Phone Number: <?php echo $rows['phone_number'];  ?> </h2>
-            <h2> Email: <?php echo $rows['email']; ?> </h2>
-            <h2> Country: <?php echo $rows['country']; ?></h2>
-			<h2> Address: <?php echo $rows['address'];  ?> </h2>
-            <h2> Age: <?php echo $rows['age']; ?> </h2>
+			<h4> First name: <?php echo $rows['first_name'];  ?> </h4>
+			<h4> Last Name: <?php echo $rows['last_name'];  ?>  </h4>
+            <h4> Gender: <?php echo $rows['gENDer']; ?> </h4>
+			<h4> Phone Number: <?php echo $rows['phone_number'];  ?> </h4>
+            <h4> Email: <?php echo $rows['email']; ?> </h4>
+            <h4> Country: <?php echo $rows['country']; ?></h4>
+			<h4> Address: <?php echo $rows['address'];  ?> </h4>
+            <h4> Age: <?php echo $rows['age']; ?> </h4>
         
             <div class="clearfix">
 				<a href=Updateprofile.php><button type="next" class="Addpetbtn">Update Profile</button></a>
 				<a href=Updatepassword.php><button type="next" class="Addpetbtn">Change Password</button></a>
             </div>
         </center>
+	</div>
     </body>
 </html>    
